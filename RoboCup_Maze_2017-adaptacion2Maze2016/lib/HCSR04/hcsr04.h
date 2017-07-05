2c069691a5f26dd71a32c37b0b76dfeec397c90e
Ezequiel2003 <ezecord_99@yahoo.com.ar>
2017-07-04 23:10:32 -0300

    Adaptacion2 de algoritmo de maze 2016-2017

---
+++ RoboCup_Maze_2017-adaptacion2Maze2016/lib/HCSR04/hcsr04.h
@@ -0,0 +1,51 @@
+/* File: HCSR04.h
+ * Author: Antonio Buonanno
+ *Board: STM NUCLEO F401RE,
+ *Hardware: Ultrasonic Range HC-SR04,
+ *
+ *This work derived from Arduino library,
+ *
+ * Desc: driver for HCSR04 Ultrasonic Range Finder.  The returned range
+ *       is in units of meters.
+ *
+ *
+ *
+*/
+
+/* EXAMPLE
+#include "mbed.h"
+#include "hcsr04.h"
+
+//D12 TRIGGER D11 ECHO
+   HCSR04 sensor(D12, D11); //trigger - echo
+int main() {
+    while(1) {
+
+     long distance = sensor.distance();
+      printf("distanza  %d  \n",distance);
+      wait(1.0); // 1 sec
+
+    }
+}
+*/
+#ifndef hcsr04_H
+#define hcsr04_H
+#include "mbed.h"
+
+
+
+class HCSR04 {
+  public:
+    HCSR04(PinName t, PinName e);
+    long echo_duration();
+    long distance();
+    long distance_mm();
+
+    private:
+        DigitalOut trig;
+        DigitalIn echo;
+        Timer timer;
+        long duration,distance_cm,distancemm;
+};
+
+#endif
